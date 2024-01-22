d31925772cac03990f14d79d5b43ddc77b5886a9
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2024-01-22 04:08:43 +0000

    uuid instalado e update e find criadas

--- core/crud.ts
+++ core/crud.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { v4 as uuid } from "uuid"
 
 const DB_FILE_PATH = "./core/db";
 
@@ -8,36 +9,59 @@ function createDB() {
   fs.writeFileSync(DB_FILE_PATH, JSON.stringify({}));
 }
 
-
 interface Todo {
- date: string
- content: string
- done: boolean
+  id: string;
+  date: string;
+  content: string;
+  done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): Todo {
   const todo: Todo = {
+    id: uuid(),
     date: new Date().toISOString(),
     content,
-    done: false
-  }
+    done: false,
+  };
 
-  const todos: Array<Todo> = [
-    ...read(),
-    todo
-  ]
-
+  const todos: Array<Todo> = [...read(), todo];
   fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2));
+
+  return todo;
 }
 
 function read(): Array<Todo> {
-  const dbString: string = fs.readFileSync(DB_FILE_PATH, "utf-8")
-  const db = JSON.parse(dbString)
-  if(!db.todos) return []  
-  return db.todos
+  const dbString: string = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString);
+  if (!db.todos) return [];
+  return db.todos;
+}
+
+function find(id: string): Todo | {} {
+  const allTodos = read()
+  const todo = allTodos.find(todo => todo.id == id)
+  return todo ? todo : {}
+}
+
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+  const allTodos = read()
+  const indexTodo = allTodos.findIndex(todo => todo.id == id)
+  
+  const todoNotExist = indexTodo == -1
+  if(todoNotExist) throw new Error("Invalid ID");  
+
+  const todoUpdated = Object.assign(allTodos[indexTodo], partialTodo)
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos: allTodos }, null, 2));
+  return todoUpdated
+}
+
+function updateContentById(id: string, content: string): Todo {
+  return update(id, { content })
 }
 
 createDB();
-create("Concluir create")
-create("Concluir read")
-console.log(read())
+create("Concluir create");
+create("Concluir read");
+const todo = create("Concluir");
+update(todo.id, { content: "Concluir up" })
+updateContentById(todo.id,  "Concluir update")
